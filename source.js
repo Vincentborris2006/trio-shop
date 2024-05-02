e122b756ead5abf35ba9870b302b6a88445cd144
Vincentborris2006 <164650139+Vincentborris2006@users.noreply.github.com>
2024-05-02 14:25:22 +0800

    Add files via upload

---
+++ source.js
@@ -0,0 +1,113 @@
+function func(){
+    var email = document.getElementById("username").value;
+    var pass  = document.getElementById("password").value;
+     if (email == 'thetrio@gmail.com' && pass == 'trio123456'){
+         window.location.assign("home.html")
+     }
+    else{
+        let user = "<br> Incorrect username and password";
+        document.getElementById("correct").innerHTML = user;
+       }
+    }
+   function Buyers(){
+    window.alert("Your order Submited");
+   }
+   function Add(){
+    window.alert("add to cart submited");
+   }
+
+    function myPaul(){
+        document.getElementById("white shirt.jpg").src ="white shirt.jpg";
+    }
+    
+    function myFunction(){
+        document.getElementById("white shirt.jpg").src="black shirt.jpg";
+    }
+
+    function myBlack(){
+        document.getElementById("wen polo.jpg").src ="wen polo.jpg";
+    }
+    
+    function myRed(){
+        document.getElementById("wen polo.jpg").src="women polo.webp";
+    }
+
+    function myCene(){
+        document.getElementById("tchin.jpg").src ="tchin.jpg";
+    }
+    
+    function myCine(){
+        document.getElementById("tchin.jpg").src="tsini.jpg";
+    }
+
+    function myBuy(){
+        document.getElementById("buy 1 balack white.jpg").src ="buy 1 balack white.jpg";
+    }
+    
+    function mySell(){
+        document.getElementById("buy 1 balack white.jpg").src="buy 1 brown.jpg";
+    }
+
+    function myPants(){
+        document.getElementById("White pants.jpg").src ="White pants.jpg";
+    }
+    
+    function myPant(){
+        document.getElementById("White pants.jpg").src="black pants.jpg";
+    }
+
+    function myEar(){
+        document.getElementById("ear black.jpg").src ="ear black.jpg";
+    }
+    
+    function myEart(){
+        document.getElementById("ear black.jpg").src="ear white.jpg";
+    }
+
+    function myHoodie(){
+        document.getElementById("hoodie black.jpg").src ="hoodie black.jpg";
+    }
+    
+    function myJacket(){
+        document.getElementById("hoodie black.jpg").src="hoodie cream.jpg";
+    }
+
+    function myBlk(){
+        document.getElementById("short black.jpg").src ="short black.jpg";
+    }
+    
+    function myCrm(){
+        document.getElementById("short black.jpg").src="short cream.jpg";
+    }
+
+    function myCar(){
+        document.getElementById("cargo cream.jpg").src ="cargo cream.jpg";
+    }
+    
+    function myGoo(){
+        document.getElementById("cargo cream.jpg").src="cargo blk.jpg";
+    }
+
+    function myJack(){
+        document.getElementById("jacket blk.jpg").src ="jacket blk.jpg";
+    }
+    
+    function myJac(){
+        document.getElementById("jacket blk.jpg").src="jacket whit.jpg";
+    }
+
+    function myTox(){
+        document.getElementById("toxic khaki.jpg").src ="toxic khaki.jpg";
+    }
+    
+    function myXic(){
+        document.getElementById("toxic khaki.jpg").src="toxic blk.jpg";
+    }
+
+    function myNobi(){
+        document.getElementById("nobi blk.jpg").src ="nobi blk.jpg";
+    }
+    
+    function myBita(){
+        document.getElementById("nobi blk.jpg").src="nobi white.jpg";
+    }
